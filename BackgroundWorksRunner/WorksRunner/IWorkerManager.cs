16d71839e1a44df5fb1c7c834d48e0a2624eb3a3
Cleomar Cantil <cleomarcantil@gmail.com>
2024-10-17 16:32:02 -0300

    Atualizações.

--- BackgroundWorksRunner/WorksRunner/IWorkerManager.cs
+++ BackgroundWorksRunner/WorksRunner/IWorkerManager.cs
@@ -3,12 +3,21 @@
 public interface IWorkerManager
 {
     /// <summary>
-    /// Adiciona uma tarefa para executar em background
+    /// Adiciona uma tarefa de um tipo para executar em background
     /// </summary>
     /// <typeparam name="T">Tipo que implementa IWorkRunner</typeparam>
     /// <param name="startDelay">Tempo de atraso até o início</param>
     /// <param name="repeatInterval">Intervalo para repetição</param>
     void AddToRun<T>(int startDelay = 0, int? repeatInterval = null) where T : IWorkRunner;
+
+    /// <summary>
+    /// Adiciona uma instância de um tipo para executar em background
+    /// </summary>
+    /// <typeparam name="T">Tipo que implementa IWorkRunner</typeparam>
+    /// <param name="instance">Instância</param>
+    /// <param name="startDelay">Tempo de atraso até o início</param>
+    /// <param name="repeatInterval">Intervalo para repetição</param>
+    void AddToRun<T>(T instance, int startDelay = 0, int? repeatInterval = null) where T : IWorkRunner;
 
     (bool Running, string Info, int? Progress) GetStatusInfo(string key);
 
